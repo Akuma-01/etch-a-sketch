ce4b04ca51596cbec842bc7214e44b5038883b93
Anurag Shah <anuragshah2024@gmail.com>
2024-06-04 01:10:16 +0530

    Do some js and css work

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+let rows = 16;
 
+let gridSize = document.querySelector("#sqps");
+const reset = document.querySelector("#reset");
 const container = document.querySelector(".container");
 
-for (let row = 1; row <= rows; row++)
-{
-    let createRow = document.createElement("div");
-    createRow.className = "row";
-    container.appendChild(createRow);
-
-    for (let column = 1; column <= rows; column++)
+let createGrid = () => {
+    for (let row = 1; row <= rows; row++)
     {
-        let createColumn = document.createElement("div");
-        createColumn.className = "grids";
-        createRow.appendChild(createColumn);
+        let createRow = document.createElement("div");
+        createRow.className = "row";
+        container.appendChild(createRow);
+
+        for (let column = 1; column <= rows; column++)
+        {
+            let createColumn = document.createElement("div");
+            createColumn.className = "grids";
+            createRow.appendChild(createColumn);
+        }
     }
 }
+
+const userInput = () => {
+    rows = prompt("Number of squares per side: ");
+    while (rows > 100)
+    {
+        alert("Enter a number less than 101!");
+        rows = prompt("Number of squares per side (less than 100): ");
+    }
+    while (rows < 1)
+    {
+        alert("Enter a number greater than 0!");
+        rows = prompt("Number of squares per side (greater than 0): ");
+    }
+    return rows;
+}
+
+function finalGrid(userInput()) {
+    createGrid;
+}
+
+gridSize.addEventListener("click", finalGrid);
+
+
+
