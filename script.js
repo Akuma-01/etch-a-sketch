30a6c58e37df3758f520c97f18c9ef275f7cf8df
Anurag Shah <anuragshah2024@gmail.com>
2024-06-04 15:26:52 +0530

    complete the etch-a-sketch project

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-let rows = 16;
-
-let gridSize = document.querySelector("#sqps");
-const reset = document.querySelector("#reset");
 const container = document.querySelector(".container");
+let newGridButton = document.querySelector("#sqps");
+const reset = document.querySelector("#reset");
 
-let createGrid = () => {
-    for (let row = 1; row <= rows; row++)
+let randomNum = () => {
+    return Math.floor(Math.random() * 256);
+}
+
+let randomColor = () => {
+    return `rgb( ${randomNum()}, ${randomNum()}, ${randomNum()} )`;
+}
+
+let grids = (row) => {
+    for (let i = 1; i <= row; i++)
     {
         let createRow = document.createElement("div");
         createRow.className = "row";
         container.appendChild(createRow);
 
-        for (let column = 1; column <= rows; column++)
+        for (let j = 1; j <= row; j++)
         {
-            let createColumn = document.createElement("div");
-            createColumn.className = "grids";
-            createRow.appendChild(createColumn);
+            let grids = document.createElement("div");
+            grids.className = "grids";
+            grids.setAttribute( "style", "background-color: white");
+            createRow.appendChild(grids);   
         }
     }
 }
 
-const userInput = () => {
+let userInput = () => {
     rows = prompt("Number of squares per side: ");
+    if (rows == '')
+    {
+        alert("Reclick Size, if you want to set size!");
+        return 16;
+    }
     while (rows > 100)
     {
         alert("Enter a number less than 101!");
@@ -35,11 +47,25 @@ const userInput = () => {
     return rows;
 }
 
-function finalGrid(userInput()) {
-    createGrid;
-}
+newGridButton.addEventListener("click", () => {
+    container.replaceChildren();
+    grids(userInput());
+});
 
-gridSize.addEventListener("click", finalGrid);
+container.addEventListener("mouseover", (event) => {
+    let target = event.target;
+    target.style.backgroundColor = randomColor(); 
+})
+
+reset.addEventListener("click", () => {
+    container.replaceChildren();
+    grids(16);
+})
+
+grids(16);
+
+
+
 
 
 
